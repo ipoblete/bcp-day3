d026fceeeeedd0a841b7ece39af4ed73c1b283ed
Ivan <i.poblete915@gmail.com>
2018-10-03 20:29:57 -0700

    added four loop

--- index2.js
+++ index2.js
@@ -1,1 +1,16 @@
-console.log('hello bc preppers!');
+var x = 12;
+
+if(x % 2 === 0) {
+    console.log(x, 'is even!');
+}
+else {
+    console.log(x, 'is odd');
+}
+
+for(var i = 0; i < 100; i++) {
+    if(i % 2 === 0) {
+        console.log(i, 'is even!');
+    }
+}    
+
+
